ea5a0daa1491abfacad7432b68d6b287d3087b6e
Idrisi Shoeb <idrisimohdshoeb@gmail.com>
2024-02-22 14:46:58 +0530

    createad pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SiginIn'
+import SignUp from './pages/SignUp'
+import Dashboard from './pages/Dashboard'
+import Projects from './pages/Projects'
 
 function App() {
 
 
   return (
-    <>
-        <h1 className="text-3xl font-bold underline text-red-300">
-      Hello world!
-    </h1>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/dashboard' element={<Dashboard/>} /> 
+        <Route path='/projects' element={<Projects/>} /> 
+      </Routes>
+    </Router>
   )
 }
 
